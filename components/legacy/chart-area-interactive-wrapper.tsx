259beaf87ae4c7f353e1e5d0a757030268a81751
pranabgoyal <contactpranabgoyal@gmail.com>
2026-02-09 01:22:21 +0530

    Fix: Delete legacy folder correctly

--- components/legacy/chart-area-interactive-wrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-export const ChartAreaInteractive = dynamic(
-  () => import("@/components/chart-area-interactive").then((mod) => mod.ChartAreaInteractive),
-  { ssr: false }
-)
